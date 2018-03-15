36fc90eb2a5cc54fcf98301bf10a3a8ccc639a0f
Anton Levchenko <levchik03021998@gmail.com>
2018-03-15 16:24:03 +0200

    header, about, cooperation sections were done

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
 
         autoprefixer: {
             options: {
-                browsers: ['last 2 versions', 'ie 8', 'ie 9']
+                browsers: ['last 2 versions', 'ie 10']
             },
             style: {
                 src: "css/style.css"
